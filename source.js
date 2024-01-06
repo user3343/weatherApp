fad427275983b294d83e26380c3a211e1a3ae027
Baljot Singh <baljotsingh3343@gmail.com>
2024-01-06 15:41:16 +0530

    ready

--- source.js
+++ source.js
@@ -1,12 +1,43 @@
 const apiKey = "f4a56f3f894c385f83802c5160ad09ea";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=delhi";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric";
 
-async function checkWeather() {
-  const response = await fetch(apiUrl + `&appid =${apiKey}`);
+const weatherIcon = document.querySelector(".weather-img img");
+const searchBox = document.querySelector(".search input");
+
+const searchBtn = document.querySelector(".search button");
+let cityName = document.querySelector("input").innerText.value;
+
+async function checkWeather(cityName) {
+  const response = await fetch(apiUrl + `&q=${cityName}` + `&appid=${apiKey}`);
   var data = await response.json();
 
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  }
   console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + `°c`;
+  document.querySelector(".humidity").innerHTML = data.main.humidity + `%`;
+  document.querySelector(".wind").innerHTML = data.wind.speed + ` km/h`;
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "./images/clouds.png";
+  } else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "./images/clear.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "./images/rain.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "./images/drizzle.png";
+  } else if (data.weather[0].main == "Mist" || "Smoke") {
+    weatherIcon.src = "./images/mist.png";
+  }
+  document.querySelector(".error").style.display = "none";
+
+  document.querySelector(".weather").style.display = "block";
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
